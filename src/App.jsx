1bdbd312e2c8995298ad88a1921716908c8a34c6
Arpit <arpitsharma6545@gmail.com>
2025-07-10 00:02:36 +0530

    Shree ganesh ji

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-black">
+      {/* Navbar */}
+      <nav className="flex justify-between items-center px-6 py-4 ">
+        <div className="md:text-2xl font-bold text-white relative">
+          OVERLAYED
+          <div className="absolute -bottom-1 left-0 w-full h-1 rounded-full"></div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <a
+            href="#"
+            className="bg-white text-black px-4 py-2 rounded-md border border-black hover:bg-gray-200 text-lg font-medium"
+          >
+            Pricing
+          </a>
+          <button className="bg-white text-black px-4 py-2 rounded-md border border-black hover:bg-gray-200 text-lg font-medium">
+            Create Account
+          </button>
+        </div>
+      </nav>
+
+      {/* Hero Section */}
+      <section className="text-center py-20 px-4 ">
+        <h1 className="text-4xl text-white md:text-6xl font-extrabold leading-tight">
+          Insert Text Behind Images with{" "}
+          <span className="text-orange-500 relative inline-block">
+            AI
+            <div className="absolute -bottom-1 left-0 w-full h-1 bg-orange-900 rounded-full"></div>
+          </span>{" "}
+        </h1>
+        <p className="mt-4 text-gray-400 text-lg">
+          Generate stunning YouTube thumbnails and content visuals that get
+          noticed.
+        </p>
+      </section>
+
+      {/* Image Grid */}
+      <section className="px-6 pb-20 bg-black">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-7xl mx-auto">
+          {[
+            {
+              src: "https://images.unsplash.com/photo-1750400519626-acbef0ec0915?auto=format&fit=crop&w=1470&q=80",
+              ratio: "landscape",
+            },
+            {
+              src: "https://images.unsplash.com/photo-1751510288461-2d088d6bf76d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              ratio: "landscape",
+            },
+            {
+              src: "https://images.unsplash.com/photo-1750400519626-acbef0ec0915?auto=format&fit=crop&w=1470&q=80",
+              ratio: "landscape",
+            },
+            {
+              src: "https://images.unsplash.com/photo-1750400519626-acbef0ec0915?auto=format&fit=crop&w=1470&q=80",
+              ratio: "landscape",
+            },
+            {
+              src: "https://images.unsplash.com/photo-1750268746263-52cdef61e177?auto=format&fit=crop&w=1398&q=80",
+              ratio: "portrait",
+            },
+            {
+              src: "https://images.unsplash.com/photo-1751510288461-2d088d6bf76d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              ratio: "landscape",
+            },
+          ].map((img, idx) => (
+            <div
+              key={idx}
+              className="overflow-hidden rounded-xl group relative bg-black"
+            >
+              <img
+                src={img.src}
+                alt={`Preview ${idx + 1}`}
+                className={`w-full object-cover transition-all duration-500 ease-in-out group-hover:scale-105 animate-fade-in ${
+                  img.ratio === "portrait" ? "h-[500px]" : "h-[300px]"
+                }`}
+                               
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [react(), tailwindcss()],
+})
